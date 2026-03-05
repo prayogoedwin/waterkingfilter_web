27e1f5d98f2b29cdb126b7e8248bfe8ad6ddedec
raka_pramu <rkapramu25@gmail.com>
2026-03-05 10:23:30 +0700

    update password partner

--- app/Filament/Resources/Partners/Tables/PartnersTable.php
+++ app/Filament/Resources/Partners/Tables/PartnersTable.php
@@ -2,13 +2,19 @@
 
 namespace App\Filament\Resources\Partners\Tables;
 
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Notifications\Notification;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
 
 class PartnersTable
 {
@@ -32,6 +38,55 @@ class PartnersTable
             ])
             ->recordActions([
                 EditAction::make(),
+                Action::make('update_password')
+                    ->label('Ubah Password')
+                    ->icon('heroicon-o-key')
+                    ->color('warning')
+                    ->closeModalByClickingAway(false)
+                    ->form([
+                        Toggle::make('reset_to_email')
+                            ->label('Reset ke Email')
+                            ->default(true)
+                            ->live()
+                            ->helperText('Password akan di-set sama dengan email user'),
+
+                        TextInput::make('custom_password')
+                            ->label('Password Baru')
+                            ->password()
+                            ->minLength(8)
+                            ->visible(fn(Get $get) => !$get('reset_to_email'))
+                            ->required(fn(Get $get) => !$get('reset_to_email'))
+                            ->revealable()
+                            ->helperText('Minimal 8 karakter'),
+
+                        TextInput::make('confirm_password')
+                            ->label('Konfirmasi Password')
+                            ->password()
+                            ->revealable()
+                            ->same('custom_password')
+                            ->visible(fn(Get $get) => !$get('reset_to_email'))
+                            ->required(fn(Get $get) => !$get('reset_to_email'))
+                            ->dehydrated(false),
+                    ])
+                    ->action(function ($record, array $data) {
+                        $newPassword = $data['reset_to_email']
+                            ? $record->email
+                            : $data['custom_password'];
+
+                        $record->update([
+                            'password' => Hash::make($newPassword),
+                        ]);
+
+                        Notification::make()
+                            ->title('Password Berhasil Diubah')
+                            ->body(
+                                $data['reset_to_email']
+                                    ? 'Password untuk ' . $record->name . ' telah direset ke: ' . $record->email
+                                    : 'Password untuk ' . $record->name . ' telah diperbarui.'
+                            )
+                            ->success()
+                            ->send();
+                    }),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
